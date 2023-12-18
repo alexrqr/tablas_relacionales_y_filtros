259068712ab4507ebe05d60244610f65c488195c
alexrqr <jeanalexromero200@gmail.com>
2023-12-18 14:48:25 -0500

    modificacion navbar

--- src/main/java/com/tablas/relacionales/controller/AppControllerAll.java
+++ src/main/java/com/tablas/relacionales/controller/AppControllerAll.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppControllerAll {
-    @GetMapping("")
+    @GetMapping("/index")
     public String inicio_web(){
         return "index";
     }
