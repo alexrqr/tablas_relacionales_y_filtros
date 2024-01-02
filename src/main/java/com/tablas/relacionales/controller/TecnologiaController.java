345a5adea858eb08fda396e65bfc88cb0bcfa2b0
alexrqr <jeanalexromero200@gmail.com>
2024-01-02 10:08:35 -0500

    Modificacion controllerTecnologia

--- src/main/java/com/tablas/relacionales/controller/TecnologiaController.java
+++ src/main/java/com/tablas/relacionales/controller/TecnologiaController.java
@@ -7,12 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TecnologiaController {
@@ -43,4 +43,11 @@ public class TecnologiaController {
 
         return "redirect:/tecnologia";
     }
+    
+    
+    @GetMapping("/delete")
+    public String delete_registro(){
+        return "redirect:/tecnologia";
+    }
+    
 }
